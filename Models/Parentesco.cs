3e059455dbc4df0d8c1fd999bc8dafa1fae8af41
Josue Marinero <josue.marinero@gmail.com>
2018-08-28 17:13:22 +0200

    parentesco

---
+++ Models/Parentesco.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GineSys.API.Models
+{
+    public class Parentesco
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Descripcion es requerido")]
+        [MaxLength(255)]
+        public string Descripcion { get; set; }
+        [MaxLength(20)]        
+        public string UsuarioCreacion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        [MaxLength(20)]        
+        public string UsuarioModificacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+    }
+}
